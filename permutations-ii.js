bbc5bb51f1a596f4bd50efe2be902899dc8053b7
Pavel <devwork.trifonov@gmail.com>
2021-12-23 17:45:43 +0300

    not working prototype created

--- permutations-ii.js
+++ permutations-ii.js
@@ -15,6 +15,28 @@ var permuteUnique = function (nums) {
 		return numberOfCombineVariants;
 	};
 
+	const getAllPermutations = (nums) => {
+		const stringPermutations = new Set();
+
+		const numsLength = nums.length;
+
+		let currentElementIndex = 0;
+
+		const getCurrentElementPermutations = (sequence, currentIndex) => {
+			const localSequence = [...sequence];
+			const currentElement = sequence[currentIndex];
+			sequence.splice(
+				currentIndex,
+				numsLength - currentIndex - 1,
+				sequence.slice(currentIndex + 1)
+			);
+			sequence.unshift(currentElement);
+			for (let index = 0; index < array.length; index++) {
+				const element = array[index];
+			}
+		};
+	};
+
 	for (let index = nums.length - 1; index >= 0; index--) {
 		const tempNums = [...nums];
 		for (let index = 0; index < tempNums.length; index++) {}
